eada441621680381921c155c3cf7c5eae2b23017
a01029143 <a010291443@tec.mx>
2025-05-30 17:42:26 -0600

    run build para los archivos de las pruebas

--- 501/vite-project/src/class8/a01029079/components/TravelRequestForm.test.js
+++ 501/vite-project/src/class8/a01029079/components/TravelRequestForm.test.js
@@ -4,17 +4,17 @@ import '@testing-library/jest-dom';
 import TravelRequestForm from './TravelRequestForm';
 test('renders the travel request form', () => {
     const { getByPlaceholderText } = render(_jsx(TravelRequestForm, {}));
-    expect(getByPlaceholderText('Destination')).toBeInTheDocument();
-    expect(getByPlaceholderText('Start Date')).toBeInTheDocument();
-    expect(getByPlaceholderText('End Date')).toBeInTheDocument();
-    expect(getByPlaceholderText('Purpose')).toBeInTheDocument();
+    expect(getByPlaceholderText('Destination'));
+    expect(getByPlaceholderText('Start Date'));
+    expect(getByPlaceholderText('End Date'));
+    expect(getByPlaceholderText('Purpose'));
 });
 test('displays validation errors if the form is submitted with invalid data', async () => {
     const { getByText, findByText } = render(_jsx(TravelRequestForm, {}));
     fireEvent.click(getByText('Submit'));
     // Usa findByText para esperar errores async
-    expect(await findByText('Destination is required')).toBeInTheDocument();
-    expect(await findByText('Start date is required')).toBeInTheDocument();
-    expect(await findByText('End date is required')).toBeInTheDocument();
-    expect(await findByText('Purpose is required')).toBeInTheDocument();
+    expect(await findByText('Destination is required'));
+    expect(await findByText('Start date is required'));
+    expect(await findByText('End date is required'));
+    expect(await findByText('Purpose is required'));
 });
