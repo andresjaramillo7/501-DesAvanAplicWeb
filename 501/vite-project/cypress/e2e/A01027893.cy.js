5734435914afde3f99494547502a0b3c1d02ff4c
Porto1090 <A01027893@tec.mx>
2025-05-30 15:17:11 -0600

    feat: add new cypress test form

--- 501/vite-project/cypress/e2e/A01027893.cy.js
+++ 501/vite-project/cypress/e2e/A01027893.cy.js
@@ -1,6 +1,6 @@
 describe('Login Page', () => {
     it('should log in with valid credentials', () => {
-        cy.visit('/http://localhost:5173/src/class9/a01029079/public/class9.html');
+        cy.visit('/src/class9/a01029079/public/class9.html');
         cy.get('input[placeholder="Email"]').type('admin');
         cy.get('input[placeholder="Contraseña"]').type('admin');
         cy.get('button[type="submit"]').click();
@@ -8,11 +8,11 @@ describe('Login Page', () => {
     });
 
     it('should display an error message with invalid credentials', () => {
-        cy.visit('/http://localhost:5173/src/class9/a01029079/public/class9.html');
+        cy.visit('/src/class9/a01029079/public/class9.html');
         cy.get('input[placeholder="Email"]').type('wronguser');
-        cy.get('input[placeholder="Contraseña"]').type('wronguser');
+        cy.get('input[placeholder="Contraseña"]').type('wrongpass');
         cy.get('button[type="submit"]').click();
-        cy.url().should('include', '/dashboard');
+        cy.get('.error').should('contain', 'Correo o contraseña inválidos');
       });
 });
   
